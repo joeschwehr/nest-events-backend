a1e7bdedea7e92134362f0f4e787e600b544ffe6
Joe Schwehr <joe.schwehr@gmail.com>
2024-09-10 19:01:16 +0100

    connect to mysql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
+import { Event } from './event.entity';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EventsController } from './events.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { EventsModule } from './events/events.module';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: '127.0.0.1',
+    port: 3306,
+    username: 'root',
+    password: 'example',
+    database: 'nest-events',
+    entities: [Event],
+    synchronize: true,
+  }),
+  TypeOrmModule.forFeature([Event]),
+  EventsModule
+  ],
   controllers: [AppController, EventsController],
   providers: [AppService],
 })
